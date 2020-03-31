eeaca1ff85413c1f0012e5130396dd11633545e3
mattfel <feldman.matthew1@gmail.com>
2020-03-30 21:41:53 -0700

    update

--- transfer.sh
+++ transfer.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # $1 = base directory with data
-rsync -avzh $1/plots/*js geoscents.net:~/visualization/
-rsync -avzh $1/plots/*html geoscents.net:~/visualization/
-rsync -avzh $1/plots/*css geoscents.net:~/visualization/
-rsync -avzh $1/plots/*jpg geoscents.net:~/visualization/
-rsync -avzh $1/plots/*gif geoscents.net:~/visualization/
+scp $1/plots/*js geoscents.net:~/visualization/
+scp $1/plots/*html geoscents.net:~/visualization/
+scp $1/plots/*css geoscents.net:~/visualization/
+scp $1/plots/*jpg geoscents.net:~/visualization/
+scp $1/plots/*gif geoscents.net:~/visualization/
+
+rsync --bwlimit=50000 -avzh $1/plots/*js geoscents.net:~/visualization/
+rsync --bwlimit=50000 -avzh $1/plots/*html geoscents.net:~/visualization/
+rsync --bwlimit=50000 -avzh $1/plots/*css geoscents.net:~/visualization/
+rsync --bwlimit=50000 -avzh $1/plots/*jpg geoscents.net:~/visualization/
+rsync --bwlimit=50000 -avzh $1/plots/*gif geoscents.net:~/visualization/
 
