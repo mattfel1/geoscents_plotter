34812762d5610b6246aa1579295e88bf771928cd
mattfel <feldman.matthew1@gmail.com>
2020-03-31 10:58:09 -0700

    compress b4 send

--- transfer.sh
+++ transfer.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# $1 = base directory with data
-scp $1/plots/*js geoscents.net:~/visualization/
-scp $1/plots/*html geoscents.net:~/visualization/
-scp $1/plots/*css geoscents.net:~/visualization/
-scp $1/plots/*jpg geoscents.net:~/visualization/
-scp $1/plots/*gif geoscents.net:~/visualization/
+scp /home/mattfel/plots/*js geoscents.net:~/visualization/
+scp /home/mattfel/plots/*html geoscents.net:~/visualization/
+scp /home/mattfel/plots/*css geoscents.net:~/visualization/
+scp /home/mattfel/plots/*jpg geoscents.net:~/visualization/
+for i in /home/mattfel/plots/*.gif; do
+    [ -f "$i" ] || break
+    [[ $(find $i -type f -size +1000000c 2>/dev/null) ]] && echo "compress $i" && gifsicle -i $i -O3 --colors 256 -o $i || echo "$i already compressed!"
+done
+scp /home/mattfel/plots/*gif geoscents.net:~/visualization/
 
-rsync --bwlimit=50000 -avzh $1/plots/*js geoscents.net:~/visualization/
-rsync --bwlimit=50000 -avzh $1/plots/*html geoscents.net:~/visualization/
-rsync --bwlimit=50000 -avzh $1/plots/*css geoscents.net:~/visualization/
-rsync --bwlimit=50000 -avzh $1/plots/*jpg geoscents.net:~/visualization/
-rsync --bwlimit=50000 -avzh $1/plots/*gif geoscents.net:~/visualization/
+#rsync --bwlimit=50000 -avzh $1/plots/*js geoscents.net:~/visualization/
+#rsync --bwlimit=50000 -avzh $1/plots/*html geoscents.net:~/visualization/
+#rsync --bwlimit=50000 -avzh $1/plots/*css geoscents.net:~/visualization/
+#rsync --bwlimit=50000 -avzh $1/plots/*jpg geoscents.net:~/visualization/
+#rsync --bwlimit=50000 -avzh $1/plots/*gif geoscents.net:~/visualization/
 
