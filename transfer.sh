64203e4f74148bd292bbedbf20c7cab271dce527
mattfel <feldman.matthew1@gmail.com>
2020-03-30 17:21:52 -0700

    update transfer

--- transfer.sh
+++ transfer.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # $1 = base directory with data
-rsync -avzh $1/plots/* geoscents.net:~/visualization/
+rsync -avzh $1/plots/*js geoscents.net:~/visualization/
+rsync -avzh $1/plots/*html geoscents.net:~/visualization/
+rsync -avzh $1/plots/*css geoscents.net:~/visualization/
+rsync -avzh $1/plots/*png geoscents.net:~/visualization/
+rsync -avzh $1/plots/*gif geoscents.net:~/visualization/
+
